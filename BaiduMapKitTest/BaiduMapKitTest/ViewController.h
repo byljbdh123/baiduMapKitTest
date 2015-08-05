b0d150afaf4a2690ea6142103414d6c40da6f10d
byljbdh <alan@luojunbindeMacBook-Pro.local>
2015-08-05 19:21:07 +0800

    init

---
+++ BaiduMapKitTest/BaiduMapKitTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  BaiduMapKitTest
+//
+//  Created by 罗骏彬 on 15/8/5.
+//  Copyright (c) 2015年 罗骏彬. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ BaiduMapKitTest/BaiduMapKitTest/ViewController.m
@@ -0,0 +1,21 @@
+//
+//  ViewController.m
+//  BaiduMapKitTest
+//
+//  Created by 罗骏彬 on 15/8/5.
+//  Copyright (c) 2015年 罗骏彬. All rights reserved.
+//  百度地图测试
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+}
+
+@end
